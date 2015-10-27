d837030e99f5e116ae871ff930b36c9626512469
antonoctopodmac <a.grechushnikov@octopod.com>
2015-10-28 02:06:53 +0300

    first commit

---
+++ views/HelloWorldViewController.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+-(IBAction)showMessage;
+
+@end
---
+++ views/HelloWorldViewController.m
@@ -0,0 +1,27 @@
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showMessage
+{
+    UIAlertView *helloWorldAlert = [[UIAlertView alloc]
+                                    initWithTitle:@"My First App" message:@"Hello, World!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    // Display the Hello World Message
+    [helloWorldAlert show];
+}
+
+@end
